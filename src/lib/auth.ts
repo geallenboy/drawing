773d3cf48ad470cbc58f702f87358dfadfdaceed
gejialun <gejialun88@163.com>
2025-02-17 14:13:47 +0800

    update

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,11 +1,9 @@
 
 "use client"
-import { useRouter } from "next/navigation";
 import { createClient } from "./supabase/client"
 
-
 const signInWith = (provider: any) => async () => {
-    const router = useRouter();
+
     const supabase = await createClient()
     const auth_callback_url = `${process.env.NEXT_PUBLIC_SITE_URL}/auth/callback`
     const { data, error } = await supabase.auth.signInWithOAuth({
@@ -19,7 +17,8 @@ const signInWith = (provider: any) => async () => {
     if (error) {
         console.log(error)
     }
-    router.push(data.url as string);
+
+    window.location.href = data.url as string
 
 }
 
